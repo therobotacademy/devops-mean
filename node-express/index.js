5841243985be55c77ee27d7b67d2b658b757f071
brjapon <brjapon@therobotacademy.com>
2018-07-26 19:00:26 +0200

    Assignment	1	Tasks	2

--- node-express/index.js
+++ node-express/index.js
@@ -1,30 +1,33 @@
 const express = require('express'),
      http = require('http');
-//...1.7.d
+
 const morgan = require('morgan');
-//...
-//...1.7.h
 const bodyParser = require('body-parser');
-//--------------------------------------------------
+
 const dishRouter = require('./routes/dishRouter');
-//. . .
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-//. . .1.7.d
 app.use(morgan('dev'));
-//. . . 1.7.h
 app.use(bodyParser.json());
-//. . .
 
-//. . . 1.7.h
-//--------------------------------------------------
+// Dish Router
 app.use('/dishes/', dishRouter);
 app.use('/dishes/:dishId', dishRouter);
 app.all('/dishes', (req,res,next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+// Promo Router
+app.use('/promos/', promoRouter);
+app.use('/promos/:romoId', promoRouter);
+app.all('/promos', (req,res,next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   next();
@@ -37,9 +40,8 @@ app.use((req, res, next) => {
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-//. . .1.7.d
+
 app.use(express.static(__dirname + '/public'));
-//. . .
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
